14a18a7bceed41815a3a8acd0c14f50a8d222fbf
Pawel Switalla <pawel.switalla@gmail.com>
2020-01-27 14:23:40 +0100

    init

---
+++ src/main/java/dk/bec/springcoretraining/SpringCoreTrainingApplication.java
@@ -0,0 +1,15 @@
+package dk.bec.springcoretraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.ws.rs.ApplicationPath;
+
+@SpringBootApplication
+public class SpringCoreTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/bec/springcoretraining/SpringCoreTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.bec.springcoretraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
